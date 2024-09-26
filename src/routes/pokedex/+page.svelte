56d325cb804cb838547b831f4889953e7a07157f
alessandro.lapini <alessandro.lapini@conjurer.es>
2024-09-26 13:00:51 +0200

    Add styling

--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -2,14 +2,15 @@
 	import { onMount } from "svelte";
 	import { getAllPokemon } from "../../api/requests/pokeRequests";
 	import type { Pokemon } from "../../interfaces/pokemon";
+	import pokeballIcon from "../../lib/images/pokeball.svg";
 
 	let allPokemons: Pokemon[] = [];
-	let flag: boolean = true;
+	let loading: boolean = true;
 
 	onMount(async () => {
 		const pokemons = await getAllPokemon();
 		allPokemons.push(...pokemons?.data.results);
-		flag = false;
+		loading = false;
 	});
 	console.log(allPokemons);
 </script>
@@ -21,17 +22,42 @@
 
 <div class="text-column">
 	<h1>Pokédex</h1>
-	{#if flag}
+	{#if loading}
 		<span>hola</span>
 	{:else}
-		<div>
-			Pokemons
-			{#each allPokemons as pokemon, index}
-				<div>
-					<span>{pokemon.name}</span>
-					<img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt="pokemon">
+		<div class="container">
+			<div class="top">
+				<div class="block1">
+					<span class="pokedex-region">Pokédex</span>
 				</div>
-			{/each}
+				<div class="block2">
+				</div>
+			</div>
+			<div class="pokedex">
+				<img src={pokeballIcon} alt="pokeball" />
+				<div class="pokemons-wrapper">
+					{#each allPokemons as pokemon, index}
+						<div class="pokemon">
+							<img
+								src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+								alt="pokemon"
+							/>
+							<span>{pokemon.name}</span>
+						</div>
+					{/each}
+				</div>
+			</div>
+			<div class="bot">
+				<div class="block1">
+					<span class="pokedex-region">Pokémon #</span>
+				</div>
+				<div class="block2">
+				</div>
+			</div>
 		</div>
 	{/if}
 </div>
+
+<style>
+	@import "../../styles/pokemon.scss";
+</style>
